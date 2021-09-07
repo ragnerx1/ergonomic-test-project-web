fc739c91faad55e528ec925f3305661ffba82ba2
Lucas Coronel <lccoronel7@icloud.com>
2021-09-07 18:53:08 -0300

    feat: company card in home

--- web/src/components/ComponentHeader/index.tsx
+++ web/src/components/ComponentHeader/index.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import { ComponentHeaderProps } from './types';
 import { Container } from './styles';
 
-const ComponentHeader: React.FC<ComponentHeaderProps> = ({ buttomBack }) => (
-  <Container>
-    {buttomBack && (
-      <Link to="/home">
-        <FiArrowLeft size={30} color="000" />
-      </Link>
-    )}
+const ComponentHeader: React.FC<ComponentHeaderProps> = ({ buttomBack }) => {
+  const [email, setEmail] = useState('');
 
-    <h2>Olá, Lucas Coronel</h2>
-  </Container>
-);
+  useEffect(() => {
+    const name = localStorage.getItem('ergonomic@name');
+    setEmail(name!);
+  }, []);
+
+  return (
+    <Container>
+      {buttomBack && (
+        <Link to="/home">
+          <FiArrowLeft size={30} color="000" />
+        </Link>
+      )}
+
+      <h2>
+        Bem vindo(a),
+        {` ${email}!`}
+      </h2>
+    </Container>
+  );
+};
 
 export default ComponentHeader;
---
+++ web/src/pages/Company/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComponentHeader from '../../components/ComponentHeader';
+
+import { Container } from './styles';
+
+const Company: React.FC = () => (
+  <Container>
+    <ComponentHeader buttomBack />
+  </Container>
+);
+
+export default Company;
