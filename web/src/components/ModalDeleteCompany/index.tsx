5bdae7b9c0f23b86b2461b8edf3cd1bec58be535
Lucas <lccoronel7@icloud.com>
2021-10-16 17:16:14 -0300

    feat: refactor: modal delete company

--- web/src/components/ModalDeleteCompany/index.tsx
+++ web/src/components/ModalDeleteCompany/index.tsx
@@ -1,28 +1,33 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { toast } from 'react-toastify';
 
 import api from '../../services/api';
 import Button from '../Button';
-import { IModalDeleteCompany } from './types';
+import { IModalDeleteCompany, IModalDeleteCompanyActions } from './types';
 import { Container, ContainerCreateData } from './styles';
 
-const ComponentDeleteCompanyModal: React.FC<IModalDeleteCompany> = ({
-  status,
-  onPress,
-  company,
-}) => {
+const ModalDeleteCompany: React.ForwardRefRenderFunction<
+  IModalDeleteCompanyActions,
+  IModalDeleteCompany
+> = ({ company }, ref) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleVisibleModal() {
+    setIsVisible(oldValue => !oldValue);
+  }
+
+  useImperativeHandle(ref, () => ({ handleVisibleModal }));
+
   async function handleDeleteCompany() {
     try {
       const token = localStorage.getItem('ergonomic@token');
       await api.delete(`company/${company}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+        headers: { Authorization: `Bearer ${token}` },
       });
-      onPress();
+      handleVisibleModal();
     } catch (err: any) {
       toast.error('Não foi possivel excluir esta empresa');
     }
@@ -30,15 +35,15 @@ const ComponentDeleteCompanyModal: React.FC<IModalDeleteCompany> = ({
 
   return (
     <Container
-      open={status}
-      onClose={onPress}
+      open={isVisible}
+      onClose={handleVisibleModal}
       aria-labelledby="simple-modal-title"
       aria-describedby="simple-modal-description"
     >
       <ContainerCreateData>
         <section className="header">
           <h2>Excluir empresa</h2>
-          <button type="button" onClick={onPress}>
+          <button type="button" onClick={handleVisibleModal}>
             <AiFillCloseCircle size={20} />
           </button>
         </section>
@@ -51,4 +56,4 @@ const ComponentDeleteCompanyModal: React.FC<IModalDeleteCompany> = ({
   );
 };
 
-export default ComponentDeleteCompanyModal;
+export default forwardRef(ModalDeleteCompany);
--- web/src/pages/Company/index.tsx
+++ web/src/pages/Company/index.tsx
@@ -4,18 +4,18 @@ import { AiOutlineSearch, AiFillDelete, AiFillEdit } from 'react-icons/ai';
 import Button from '@components/Button';
 import Header from '@components/Header';
 import { IModalCreateCompanyActions } from '@components/ModalCreateCompany/types';
+import { IModalDeleteCompanyActions } from '@components/ModalDeleteCompany/types';
 import ModalCreateCompany from '@components/ModalCreateCompany';
-import ComponentDeleteCompanyModal from '../../components/ComponentDeleteCompanyModal';
+import ModalDeleteCompany from '@components/ModalDeleteCompany';
 import api from '../../services/api';
 import { ICompany, IListCompanies } from './types';
 import { Container } from './styles';
 
 const Company: React.FC = () => {
   const modalCreateCompany = useRef<IModalCreateCompanyActions>(null);
+  const modalDeleteCompany = useRef<IModalDeleteCompanyActions>(null);
 
   const [companies, setCompanies] = useState<ICompany[]>([]);
-  const [visibleModal, setVisibleModal] = useState(false);
-  const [visibleModalDelete, setVisibleModalDelete] = useState(false);
   const [companySelected, setCompanySelected] = useState('');
   const [serach, setSearch] = useState('');
   const [name, setName] = useState('');
@@ -29,21 +29,17 @@ const Company: React.FC = () => {
         },
       })
       .then(response => setCompanies(response.data));
-  }, [visibleModal, visibleModalDelete]);
-
-  function handleModal() {
-    setVisibleModal(oldValue => !oldValue);
-  }
+  }, []);
 
   function handleModalDelete(id: string) {
     setCompanySelected(id);
-    setVisibleModalDelete(oldValue => !oldValue);
+    modalDeleteCompany.current?.handleVisibleModal();
   }
 
   function handleModalEditModal(id: string, nameCompany: string) {
     setCompanySelected(id);
     setName(nameCompany);
-    setVisibleModal(oldValue => !oldValue);
+    modalCreateCompany.current?.handleVisibleModal();
   }
 
   const filter = (listCompanies: ICompany[], query: string) =>
@@ -103,7 +99,10 @@ const Company: React.FC = () => {
             />
           </div>
 
-          <Button title="Cadastrar nova empresa" onPress={handleModal} />
+          <Button
+            title="Cadastrar nova empresa"
+            onPress={() => modalCreateCompany.current?.handleVisibleModal()}
+          />
         </div>
 
         <section className="header-table">
@@ -130,11 +129,7 @@ const Company: React.FC = () => {
         ref={modalCreateCompany}
       />
 
-      <ComponentDeleteCompanyModal
-        status={visibleModalDelete}
-        onPress={() => handleModalDelete('')}
-        company={companySelected}
-      />
+      <ModalDeleteCompany company={companySelected} ref={modalDeleteCompany} />
     </Container>
   );
 };
