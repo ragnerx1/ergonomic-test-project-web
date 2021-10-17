61d0126c06a8d7b81e6da82d5ad1d06894b5dcad
Lucas <lccoronel7@icloud.com>
2021-10-17 07:25:45 -0300

    refactor: header

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -3,16 +3,15 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import { useAuth } from '../../hooks/auth';
 import { IHeader } from './types';
 import { Container } from './styles';
 
 const Header: React.FC<IHeader> = ({ buttomBack }) => {
+  const { user } = useAuth();
   const [email, setEmail] = useState('');
 
-  useEffect(() => {
-    const name = localStorage.getItem('ergonomic@name');
-    setEmail(name!);
-  }, []);
+  useEffect(() => setEmail(user.register.email), [user]);
 
   return (
     <Container>
