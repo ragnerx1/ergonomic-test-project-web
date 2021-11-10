2a3eeaf28ff12889be216dc61a78611780cbd2a6
Lucas Coronel <lccoronel7@icloud.com>
2021-11-10 07:58:39 -0300

    refactor: question type components

--- web/src/components/QuestionTypeOne/styles.ts
+++ web/src/components/QuestionTypeOne/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { variables } from '../../styles/variables';
 
 export const Container = styled.section`
-  margin-top: 50px;
+  margin-top: 30px;
   background-color: ${variables.colors.white_500};
   padding: 20px;
   border-radius: 7px;
