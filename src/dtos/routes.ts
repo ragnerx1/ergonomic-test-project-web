06adaa7e45c31789ff4c05cc38fe799017fdf6de
Lucas Coronel <lccoronel7@icloud.com>
2021-11-30 18:46:14 -0300

    refactr : routes

---
+++ src/dtos/routes.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-shadow */
+
+export enum ERoutes {
+  LOGIN_USER = '/',
+  LOGIN_ADMIN = '/admin',
+  HOME = '/home',
+  USERS = '/users',
+  COMPANY = '/company',
+  FORM = '/form',
+  QUESTION = '/questions',
+  USER_FORM = '/user-form',
+  USER_INFO = '/user-info',
+  DISCOMFORT_MAP = '/discomfort-map',
+}
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
+import { ERoutes } from '../dtos/routes';
 import DiscomfortMap from '../pages/DiscomfortMap';
 import GetInfoUser from '../pages/GetInfoUser';
 import UserForm from '../pages/UserForm';
 import Login from '../pages/Login';
-// import Home from '../pages/Home';
-// import Register from '../pages/Register';
-// import LoginAdmin from '../pages/LoginAdmin';
-// import Company from '../pages/Company';
-// import Form from '../pages/Form';
-// import Questions from '../pages/Questions';
+import Home from '../pages/Home';
+import { Users } from '../pages/Users';
+import LoginAdmin from '../pages/LoginAdmin';
+import Company from '../pages/Company';
+import Form from '../pages/Form';
+import Questions from '../pages/Questions';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={Login} />
-      {/* <Route path="/admin" component={LoginAdmin} />
-      <Route path="/home" component={Home} />
-      <Route path="/register" component={Register} />
-      <Route path="/company" component={Company} />
-      <Route path="/form" component={Form} />
-      <Route path="/questions" component={Questions} /> */}
-      <Route path="/user-form" component={UserForm} />
-      <Route path="/user-info" component={GetInfoUser} />
-      <Route path="/discomfort-map" component={DiscomfortMap} />
+      <Route path={ERoutes.LOGIN_USER} exact component={Login} />
+      <Route path={ERoutes.LOGIN_ADMIN} component={LoginAdmin} />
+      <Route path={ERoutes.HOME} component={Home} />
+      <Route path={ERoutes.USERS} component={Users} />
+      <Route path={ERoutes.COMPANY} component={Company} />
+      <Route path={ERoutes.FORM} component={Form} />
+      <Route path={ERoutes.QUESTION} component={Questions} />
+      <Route path={ERoutes.USER_FORM} component={UserForm} />
+      <Route path={ERoutes.USER_INFO} component={GetInfoUser} />
+      <Route path={ERoutes.DISCOMFORT_MAP} component={DiscomfortMap} />
     </Switch>
   </BrowserRouter>
 );
