af235683d012e4df9080eb97eebcb31c712b4ef8
Ragner Evangelista de Lima <68446166+ragnerx1@users.noreply.github.com>
2022-02-23 10:57:54 -0300

    New form responses
    Ainda é necessário rever as questões ID-12 e ID-13.

--- src/pages/UserForm/data.ts
+++ src/pages/UserForm/data.ts
@@ -5,7 +5,7 @@ export const form: IQuestion[] = [
     id: 0,
     question_type: 1,
     description:
-      'Qual imagem mais se assemelha ao local onde você usa o computador para trabalhar:',
+      'Qual imagem mais se assemelha ao local onde você apoia seu computador a maior parte do tempo para trabalhar?',
     first_photo:
       'https://www.moveisdevalor.com.br/portal/upload/files/image/16475/pessoa_trabalhando_na_cama.png',
     second_photo:
@@ -15,17 +15,17 @@ export const form: IQuestion[] = [
     fourth_photo:
       'https://www.meupositivo.com.br/panoramapositivo/wp-content/uploads/2019/02/original-845ef4a870d43861d9b59d340aaf9035.jpg',
     first_answer:
-      'Nesta posição você está  expondo seu corpo ao risco de desenvolver doenças osteomusculares. Procure trabalhar em uma mesa compatível com sua altura.',
+      'Há necessidade de rever o local onde você apoia seu computador ,Esta posição pode parecer prática, mas não proporciona uma acomodação confortável à sua coluna vertebral, ombros e punho do ponto de vista Ergonômico. Procure utilizar uma mesa compatível com a sua altura. Veja o vídeo ao  final da avaliação.',
     second_answer:
       'Nesta posição você está  expondo seu corpo ao risco de desenvolver doenças osteomusculares. Procure trabalhar em uma mesa compatível com sua altura.',
     third_answer:
-      'Você está utilizando uma mesa, isso é bom. Pode ser necessário alguma adaptação para estar compativel com sua altura',
-    fourth_answer: 'Está mesa é ergonômica e facilita o conforto.',
+      'Você está utilizando uma mesa, isso é bom. Pode ser necessário alguma adaptação para que esta mesa esteja compatível com sua altura proporcionando maior conforta a seus punhos e ombros.',
+    fourth_answer: 'Está mesa é ergonômica e facilita o conforto. Muito bom!',
   },
   {
     id: 1,
     question_type: 1,
-    description: 'Qual cadeira mais parece com a que você usa:',
+    description: 'Qual cadeira mais parece com a que você usa na maior parte do tempo:',
     first_photo:
       'https://a-static.mlcdn.com.br/618x463/cadeira-tramontina-diana-eco-em-polipropileno-reciclado-preto/marinbrasiltramontina/2047/971ce424b31659043ce0bd8d29c2ea18.jpg',
     second_photo:
@@ -35,13 +35,13 @@ export const form: IQuestion[] = [
     fourth_photo:
       'https://attraktiva.vteximg.com.br/arquivos/ids/191468-1000-1000/Sofa-Ohio-3-Lugares--L--208cm--100--Couro-Natural-Preto.png?v=637407086128630000',
     first_answer:
-      'Está cadeira não é adequada para trabalho diário , mas é possível, melhorar o seu conforto. Assista ao vídeo de dicas.',
+      'Há necessidade de adequar a cadeira que você está utilizando para uma com assento/ encosto  almofadados e regulagem de altura.  Assista ao vídeo de dicas ao final desta avaliação.',
     second_answer:
       'Esta cadeira é ergonômica do ponto de vista da regulagem de altura, mas se possível adquira uma que tenha suporte para braços também.',
     third_answer:
-      'Esta cadeira é ergonômica e possibilita regulagens e adequação à mesa utilizada para o  trabalho .',
+      'Esta cadeira é ergonômica e possibilita regulagens e adequação à mesa utilizada para trabalho.',
     fourth_answer:
-      'Sofás não são recomendados para serem utilizados como assento para trabalho com computador devido a não possibilitar regulagem de altura e não permitir um suporte adequado à coluna vertebral',
+      'É necessário utilizar cadeira com regulagem de altura e, se possível,  apoio para braços. Sofás não são recomendados para serem utilizados como assento para trabalho com computador devido a não possibilitar regulagem de altura e não permitir um suporte adequado à coluna vertebral.',
   },
   {
     id: 2,
@@ -56,20 +56,20 @@ export const form: IQuestion[] = [
     id: 3,
     question_type: 3,
     description:
-      'Você usa suporte ou adaptação para o monitor ou tela do notebook ficar na altura dos seus olhos, sem inclinar a cabeça',
+      'Você usa suporte ou adaptação para o monitor ou tela do notebook ficar na altura dos seus olhos, sem inclinar o pescoço?',
     multiple_choice: [
       { id: 0, choice: 'Sim' },
       { id: 1, choice: 'Não' },
     ],
     first_photo: 'https://img.kalunga.com.br/fotosdeprodutos/666032z.jpg',
     first_answer:
-      'Notebooks utilizados diretamente sobre a mesa ou outra superfície tendem a sobrecarregar o pescoço e os músculos do trapézio, predispondo à dores. Procure elevar o Notebook com suporte apropriado, livros ou caixas para que fique na altura da linha dos olhos',
-    second_answer:
       'Você está utilizando o Notebook de maneira ergonômica utilizando suporte para eleva-lo à altura dos olhos.',
-    third_answer:
-      'Utilize apoios para elevar a altura do monitor  de maneira que fique na altura da linha dos olhos.',
-    fourth_answer:
-      'Você está cuidando de seu bem estar utilizando apoio para elevar e regular a altura do monitor em relação a linha dos olhos.',
+    second_answer:
+      'Há necessidade de utilizar um suporte para Notebooks afim de proporcionar mais conforto para seus ombros e pescoço. Notebooks utilizados diretamente sobre a mesa ou outra superfície tendem a sobrecarregar o pescoço e os músculos do trapézio, predispondo à dores. Procure elevar o Notebook com suporte apropriado, livros ou caixas para que fique na altura da linha dos olhos.',
+   // third_answer:
+      //'Utilize apoios para elevar a altura do monitor  de maneira que fique na altura da linha dos olhos.',
+    //fourth_answer:
+      //'Você está cuidando de seu bem estar utilizando apoio para elevar e regular a altura do monitor em relação a linha dos olhos.',
   },
   {
     id: 4,
@@ -81,9 +81,9 @@ export const form: IQuestion[] = [
       { id: 3, choice: 'Não uso notebook' },
     ],
     first_answer:
-      'Você está trabalhando de forma ergonômica e prevenindo problemas osteomusculares',
+      'Que bom! Você está aproveitando dispositivos ergonômicos para facilitar seu trabalho e contribui com seu  bem estar.',
     second_answer:
-      'Geralmente a não utilização de teclado e mouse acoplado ao notebook predispõe a sobrecarregar o músculo do Trapézio e gerar dores nos ombros. Melhore seu conforto fazendo uso destes periféricos.',
+      ': Se faz necessário, quando se usa Notebooks, a utilização de teclado e mouse acoplados para que seu pescoço, ombros e punhos estejam mais confortáveis. Geralmente a não utilização de teclado e mouse acoplado ao notebook predispõe a sobrecarregar o músculo do Trapézio e gerar dores nos ombros. Melhore seu conforto fazendo uso destes periféricos.',
   },
   {
     id: 5,
@@ -98,7 +98,7 @@ export const form: IQuestion[] = [
     first_answer:
       'Parabéns! você está cuidando de seu punho e do Túnel do Carpo, por onde passam os tendões dos dedos da mão',
     second_answer:
-      'O mousepad auxilia manter o punho em posição neutra e prevenir tendinites e problemas com o Túnel do Carpo (por onde passam os tendões dos dedos da mão.) Procure utilizar o mousepad para seu maior conforto.',
+      'Há necessidade de se utilizar o Mousepad, ele lhe auxilia a manter o punho em posição neutra e prevenir desgastes dos tendões e do Túnel do Carpo (por onde passam os tendões dos dedos da mão.',
   },
   {
     id: 6,
@@ -112,7 +112,7 @@ export const form: IQuestion[] = [
     first_answer:
       'Uma boa iluminação favorece o conforto ao trabalhar, que bom que você cuida este aspecto!',
     second_answer:
-      'A utilização de computador em lugar com má iluminação predispõe há um maior cansaço e outros problemas.',
+      'Há necessidade de adequar a iluminação do ambiente de maneira que a luz do ambiente seja mais forte do que a luz da tela do computador para evitar  prejuízos ao sistema ocular.',
   },
   {
     id: 7,
@@ -140,7 +140,7 @@ export const form: IQuestion[] = [
     first_answer:
       'Utilizando o apoio para os pés você mantém uma melhor circulação dos membros inferiores e não sobrecarrega a articulação do joelho.',
     second_answer:
-      'A não Utilização de apoio para os pés quando seus pés ficam suspensos no ar,  acarreta compressão de vasos sanguíneos e compromete a  circulação dos membros inferiores e a  sobrecarga da articulação do joelho. Procure utilizar apoio para os pés. ',
+      'Se faz necessário a utilização de apoio para os pés  quando seus pés ficam suspensos no ar  acarreta compressão de vasos sanguíneos e compromete a  circulação dos membros inferiores e a  sobrecarga da articulação do joelho. Procure utilizar apoio para os pés.',
   },
   {
     id: 9,
@@ -153,7 +153,7 @@ export const form: IQuestion[] = [
     first_answer:
       'Seus músculos e tendões agradecem! Continue cuidando de seu corpo! Os alongamentos promovem o alinhamento das fibras musculares promovendo melhor desempenho dos músculos e tendões, melhora a circulação e acelera a metabolização do Ácido Lácteo (residuo que causa dor nos músculos) ',
     second_answer:
-      'Seus músculos e tendões precisam de reparos e descanso! Os alongamentos promovem o alinhamento das fibras musculares promovendo melhor desempenho dos músculos e tendões, melhora a circulação e acelera a metabolização do Ácido Lácteo (residuo que causa dor nos músculos). Quando alonga você prolonga a vida de seus músculos e tendões bem como de seu corpo todo!!!!',
+      'Há necessidades de se fazer pausas para alongamentos durante o dia. Seus músculos e tendões precisam de reparos e descanso! Os alongamentos promovem o alinhamento das fibras musculares promovendo melhor desempenho dos músculos e tendões, melhora a circulação e acelera a metabolização do Ácido Lácteo (resíduo que causa dor nos músculos). Quando alonga você prolonga a vida de seus músculos e tendões bem como de seu corpo todo!',
   },
   {
     id: 10,
@@ -167,7 +167,7 @@ export const form: IQuestion[] = [
     first_answer:
       'Muito bem você está permitindo que seus ombros e braços estejam em posição mais confortável.',
     second_answer:
-      'De preferência para que os objetos que mais utiliza se encontrem bem ao alcance de suas mão e procure acessa-los sem esforço. Teclado e mouse devem ficar perto da borda da mesa para facilitar o acesso e utilização mais confortável.',
+      'Procure organizar seu local de trabalho de maneira que os objetos que mais utiliza, mouse, teclado, telefone ou outro, se encontrem ao alcance de suas mãos com os braços em angulação próximo a 90° e que possa acessa-los sem esforço. Teclado e mouse devem ficar perto da borda da mesa para facilitar o acesso e utilização mais confortável.',
   },
   {
     id: 11,
@@ -178,7 +178,7 @@ export const form: IQuestion[] = [
     second_photo:
       'https://patricialacombe.com.br/blog/wp-content/uploads/2018/03/179186-voce-sabe-qual-e-a-postura-correta-para-usar-o-celular-730x340.jpg',
     first_answer:
-      'Desta forma a cabeça fica pendendo para frente e sobrecarregando ombros e pescoço, predispondo a dores. Dependendo do tempo de exposição ao celular. Procure utilizar o celular na posição do aparelho em pé como o exemplo  da figura 2 para o maior conforto das vértebras da coluna Cervical.',
+      'Se faz necessário utilizar o celular na posição vertical, evitando olhar demasiado para baixo e sobrecarregar musculatura do pescoço e ombros. Procure utilizar o celular na posição do aparelho em pé como o exemplo da figura 2 para  maior conforto da coluna cervical e toráxica.',
     second_answer:
       'Desta forma sua cabeça está apoiada adequadamente e não sobrecarrega ombros e pescoço. A Ergonomia é um valor para você!',
   },
@@ -202,7 +202,7 @@ export const form: IQuestion[] = [
       { id: 1, choice: 'Não' },
     ],
     first_answer:
-      'A água é fundamental para as funções de nosso organismo, 80% dele é líquido. A água  ',
+      'A água é fundamental para todas as funções de nosso organismo, 80% dele é líquido. A hidratação adequada influencia diretamente em nosso bem-estar e desempenho. O ideal é ingerirmos de 6-8 copos de água por dia ou mais dependendo do peso corporal e do clima.',
   },
   {
     id: 14,
